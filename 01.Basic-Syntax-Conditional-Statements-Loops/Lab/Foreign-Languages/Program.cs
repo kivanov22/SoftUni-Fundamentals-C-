e872e5d881d92f4386fbfcf0eba3c2a09c1c100e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-15 17:04:56 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Lab/Foreign-Languages/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+
+
+
+            //if(country=="USA" && country == "England")
+            //{
+            //    Console.WriteLine("English");
+            //}
+            //if(country=="Spain" && country == "Argentina" && country == "Mexico")
+            //{
+            //    Console.WriteLine("Spanish");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Unknown");
+            //}
+        }
+    }
+}
