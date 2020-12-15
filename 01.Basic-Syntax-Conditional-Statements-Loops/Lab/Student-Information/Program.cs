4f7616fa27de9c2b550b9cedee4c985a75e9a9b8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-15 17:01:16 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Lab/Student-Information/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double average = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {average:f2}");
+        }
+    }
+}
