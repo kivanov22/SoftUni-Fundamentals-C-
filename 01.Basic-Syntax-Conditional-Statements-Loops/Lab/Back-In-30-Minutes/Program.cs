c033ba6d254a77417a7c0aaabf17d31874912286
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-15 17:02:52 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Lab/Back-In-30-Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int mins = int.Parse(Console.ReadLine());
+
+            mins += 30;
+
+            if (mins >= 60)
+            {
+                hours++;
+                mins -= 60;
+                
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+                
+            }
+            Console.WriteLine($"{hours}:{mins:D2}");
+        }
+    }
+}
