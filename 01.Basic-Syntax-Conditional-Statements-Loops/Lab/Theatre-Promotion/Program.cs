e31a1d82b7ff7708471d2f2ad380558c5e71de4b
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-21 12:51:28 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Lab/Theatre-Promotion/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayType = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            switch (dayType)
+            {
+                case "Weekday":
+                    if (0 <= age && age <= 18)
+                    {
+                        price = 12;
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        price = 18;
+                    }
+                    else if (64 < age && age <= 122)
+                    {
+                        price = 12;
+                    }
+                    break;
+                case "Weekend":
+                    if (0 <= age && age <= 18)
+                    {
+                        price = 15;
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        price = 20;
+                    }
+                    else if (64 < age && age <= 122)
+                    {
+                        price = 15;
+                    }
+                    break;
+                case "Holiday":
+                    if (0 <= age && age <= 18)
+                    {
+                        price = 5;
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        price = 12;
+                    }
+                    else if (64 < age && age <= 122)
+                    {
+                        price = 10;
+                    }
+                    break;
+                
+
+            }
+            if (price != 0)
+            {
+                Console.WriteLine($"{price}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+
+            }
+
+        }
+    }
+}
