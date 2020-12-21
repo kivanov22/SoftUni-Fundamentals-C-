c22abc052f67e271f475658f7b96a54425547e43
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-21 13:16:08 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Exercise/TriangleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _8.TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
