f3dd8c917eae1fc62f6b2ed6250d8c24e1537b66
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-21 13:13:15 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Exercise/Login/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _5.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            int times = 0;
+
+            while (true)
+            {
+                string currentUser = Console.ReadLine();
+
+                if (currentUser != password)
+                {
+                    times++;
+
+                    if (times == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else 
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
