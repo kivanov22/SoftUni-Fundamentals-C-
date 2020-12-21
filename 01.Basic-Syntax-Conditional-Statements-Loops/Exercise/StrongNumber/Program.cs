8b5228bd04980beedb2fdb83f254caaabf7addfb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-21 13:14:16 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Exercise/StrongNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _6.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = n;
+
+            int currentNumber = 0;
+            int factorialSum = 0;
+
+            while (number !=0)
+            {
+                currentNumber = number % 10;
+                number /= 10;
+
+
+                int factorial = 1;
+
+                for (int i = 1; i <= currentNumber; i++)
+                {
+                    factorial *= i;
+                }
+                factorialSum += factorial;
+            }
+
+            string result = "";
+
+            if(n == factorialSum)
+            {
+                result = "yes";
+            }
+            else
+            {
+                result = "no";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
