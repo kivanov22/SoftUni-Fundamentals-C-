c23a24b51afc033f71da8b0d29219c7152265382
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-25 16:25:55 +0200

    Add files via upload

---
+++ 01.Basic-Syntax-Conditional-Statements-Loops/Additional-Exercise/2. EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _2._EnglishNameOfTheLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = long.Parse(Console.ReadLine());
+
+            LastDigit(num);
+        }
+
+        static void LastDigit(long num)
+        {
+            switch (Math.Abs(num%10))
+            {
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+
+            }
+        }
+    }
+}
