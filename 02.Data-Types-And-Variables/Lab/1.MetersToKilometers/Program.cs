dbf1a5e870a30ec1b402c19e75eae1bb446054bc
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-31 12:48:34 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/1.MetersToKilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1.MetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000.0M;
+            Console.WriteLine($"{kilometers:f2}");
+
+        }
+    }
+}
