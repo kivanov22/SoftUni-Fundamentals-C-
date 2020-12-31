42efdc9db9fabba2ca77392df0c88ed96e5e3216
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-31 12:51:00 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/5.SpecialNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _5.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            bool check = false;
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+
+                while (digits>0)
+                {
+
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+
+
+                    
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+                sumOfDigits = 0;
+                Console.WriteLine($"{i} -> {check}");
+            }
+            
+
+        }
+    }
+}
