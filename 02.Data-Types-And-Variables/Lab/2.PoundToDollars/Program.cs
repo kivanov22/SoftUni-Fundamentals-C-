edae9b11e0b3fbc17ef5aef1cb3396a087f7ce66
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-31 12:49:09 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/2.PoundToDollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2.PoundToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           decimal pound = decimal.Parse(Console.ReadLine());
+
+            decimal dollar = pound * 1.31M;
+            Console.WriteLine($"{dollar:f3}");
+        }
+    }
+}
