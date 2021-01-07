954228e09d32eb27f68e7ccc801719aedc1f3cae
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-07 23:46:01 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/6.ReversedChars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6.ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            Console.Write($"{c} {b} {a}");
+            
+        }
+    }
+}
