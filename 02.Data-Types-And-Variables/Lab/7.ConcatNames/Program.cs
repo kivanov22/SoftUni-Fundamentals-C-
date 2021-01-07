baa55a5773a4775f95178af9a15d8a30261be34c
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-07 23:46:42 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/7.ConcatNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _7.ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+
+            string result = firstName + delimeter + lastName;
+            Console.WriteLine(result);
+        }
+    }
+}
