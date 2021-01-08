a4d756c0eb29fd886896c2401fa775a23e7e89f0
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-08 16:42:40 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/10.LowerOrUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10.LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+
+            if (char.IsLower(firstChar))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else if (char.IsUpper(firstChar))
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
