ae37c25fbc83bce283c855f46a633e6eb75b13be
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:51:48 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/1.Integer-Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1.Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+
+            int first = number1 + number2;
+            int sum = first / number3;
+            int multy = sum * number4;
+
+            Console.WriteLine(multy);
+        }
+    }
+}
