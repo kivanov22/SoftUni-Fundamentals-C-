08671fd3f5215562c28eedb4df84ffd18b0a7142
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:50:01 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _11.RefactorVolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double length = double.Parse(Console.ReadLine());
+
+            
+            double width = double.Parse(Console.ReadLine());
+
+            
+           double heigth = double.Parse(Console.ReadLine());
+
+            double total = length*width*heigth/3;
+            Console.Write($"Length: Width: Height: Pyramid Volume: {total:f2}");
+
+        }
+    }
+}
