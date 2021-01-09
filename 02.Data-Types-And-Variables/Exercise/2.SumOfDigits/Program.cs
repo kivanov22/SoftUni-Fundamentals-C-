b946b6756b7b6e916cf8eb8086f5cd311ddce779
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:52:28 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/2.SumOfDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2.SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentNumber = int.Parse(input[i].ToString());
+                 sum+= currentNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
