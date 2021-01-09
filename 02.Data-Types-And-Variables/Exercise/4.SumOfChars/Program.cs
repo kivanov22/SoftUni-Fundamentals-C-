bf28b0454b5b30b45245c5d567ad0744225e7662
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:53:50 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/4.SumOfChars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4.SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());// we receive a number
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++) // loop for 0 till n times
+            {
+                char input = char.Parse(Console.ReadLine());// take a char value "A"= 65 ASCII number for example.
+                sum += input;
+                //ASCII numbers sum of the letters.
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
