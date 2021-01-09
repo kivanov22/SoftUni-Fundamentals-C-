a4680f72800c0910e9b22ce2f14c7caad616a9c3
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:50:46 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+
+            bool isSpecial = false;
+            int special = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                special = i;
+                int sum = 0;
+               
+
+                while (i > 0)
+                {
+                    sum += special % 10;
+                    special = special / 10;
+                }
+                isSpecial = sum == 5 || sum == 7 || sum == 11;
+
+                sum = 0;
+                if (isSpecial)
+                {
+                    Console.WriteLine("{0} -> {1}", special, isSpecial);
+                }
+                
+            }
+            
+        }
+        }
+}
