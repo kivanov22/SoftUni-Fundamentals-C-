61031c18d79608e041b2ae60937f06163db37e9e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-09 17:53:11 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/3.Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            while (number>0)
+            {
+                
+                number -= capacity;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
