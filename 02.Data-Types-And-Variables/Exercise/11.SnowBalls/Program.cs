7cffff33c7c538ccf7004eae00dc36877c317866
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:21:32 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/11.SnowBalls/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11.SnowBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSnowBalls = int.Parse(Console.ReadLine());
+
+            
+            int maxSnowBallSnow = 0;
+            int maxSnowBallTime = 0;
+            int maxSnowBallQuality = 0;
+            BigInteger maxSnowBallValue = 0;
+
+            for (int i = 0; i < numberSnowBalls; i++)
+            {
+              int   snowBallSnow = int.Parse(Console.ReadLine());
+              int   snowBallTime = int.Parse(Console.ReadLine());
+              int  snowBallQuality = int.Parse(Console.ReadLine());
+
+              BigInteger  snowBallValue = BigInteger.Pow((snowBallSnow / snowBallTime),snowBallQuality);//vmesto Math.Pow direktno BigInteger.Pow
+                
+                if (snowBallValue>=maxSnowBallValue)
+                {
+                    
+                    maxSnowBallSnow = snowBallSnow;
+                    maxSnowBallTime = snowBallTime;
+                    maxSnowBallQuality = snowBallQuality;
+                    maxSnowBallValue = snowBallValue;
+                }
+
+            }
+            Console.WriteLine($"{maxSnowBallSnow} : {maxSnowBallTime} = {maxSnowBallValue} ({maxSnowBallQuality})");
+
+        }
+    }
+}
