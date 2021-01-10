c2746f9c7c511c4626420f0dc1aba7d2a7ceff15
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:18:17 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/6.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6.TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstChar = (Char)(97 + i);
+                        char secondChar = (Char)(97 + j);
+                        char thirdChar = (Char)(97 + k);
+
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                        
+                       
+                       
+                    }
+                }
+            }
+
+        }
+    }
+}
