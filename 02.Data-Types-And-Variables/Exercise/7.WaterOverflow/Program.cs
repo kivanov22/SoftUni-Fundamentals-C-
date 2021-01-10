52abc0255ce53b24b010f37d1a893fcc8d478bf8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:19:11 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/7.WaterOverflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int litters = int.Parse(Console.ReadLine());
+
+               
+
+                bool isFull = litters > 255; //proverki
+                bool overFlow = sum + litters> 255;
+
+                if (isFull || overFlow)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                sum += litters;
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
