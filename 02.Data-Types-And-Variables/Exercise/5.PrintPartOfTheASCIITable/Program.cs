a2221f66bec0ea892f96ab2df40275b74b02ff6e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:17:43 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/5.PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5.PrintPartOfTheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            
+
+            for (int i = number1; i <= number2; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+           
+        }
+    }
+}
