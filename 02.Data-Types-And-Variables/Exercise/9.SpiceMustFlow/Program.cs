466be07d5fd3451493edbe6b2df60d93b91e2afb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:20:27 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/9.SpiceMustFlow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _9.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+
+            int amount = 0;
+            int days = 0;
+
+            while (startingYield>0)
+            {
+                if (startingYield < 100)
+                {
+                    if (amount<26)
+                    {
+                        break;
+                    }
+                    amount -= 26;
+                    break;
+                }
+                amount += startingYield - 26;
+                startingYield -= 10;
+                days++;
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(amount);
+        }
+    }
+}
