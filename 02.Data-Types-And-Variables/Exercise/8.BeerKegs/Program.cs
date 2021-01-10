02151c61e93f0b7aec976b3ea64594309b756f31
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-10 16:19:51 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/Exercise/8.BeerKegs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _8.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            string biggestKeg = "";
+            double biggestVolume = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                //π * r^2 * h.
+
+                double volume= Math.PI *Math.Pow(radius,2) * height;//тук ползваме библиотека за пи и Мат.Поу за повдигане на втора
+
+
+                if (volume>biggestVolume)
+                {
+                    biggestVolume= volume; // сравняваме обема на Кеговете
+                    biggestKeg = model; // и според обема , определяме името на Кега с наи-висок обем
+
+                }
+            }
+            Console.WriteLine(biggestKeg);
+
+
+
+
+        }
+    }
+}
