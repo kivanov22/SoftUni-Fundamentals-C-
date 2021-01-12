98e8db7abe9064031ff49bf78c365890c97221f9
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-12 13:49:13 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/More-Exercise/3.FloatingEquality/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3.FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double precision = 0.000001;
+
+            if (a<b)
+            {
+                double temp = a;
+                a = b;
+                b = temp;
+            }
+            if (a-b<precision)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
