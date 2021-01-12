1ad7b05ed92451c1e58a94a21305f71914378fc2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-12 13:51:55 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/More-Exercise/6.BalancedBrackets/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _6.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int openCount = 0;
+            int closeCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    openCount++;
+
+                }
+                else if (input == ")")
+                {
+                    closeCount++;
+                    if (openCount - closeCount != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            if (openCount == closeCount)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+        }
+    }
+
