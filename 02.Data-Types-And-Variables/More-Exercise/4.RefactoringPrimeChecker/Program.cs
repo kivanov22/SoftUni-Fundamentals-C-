9c9560366c9c7a0dd1047fd757614ee655650854
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-12 13:50:08 +0200

    Add files via upload

---
+++ 02.Data-Types-And-Variables/More-Exercise/4.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4.RefactoringPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string isPrime = "";
+            for (int i = 2; i <= number; i++)
+            {
+                //bool isPrime = true;
+                isPrime = "true";
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        //isPrime = false;
+                        isPrime = "false";
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime}");
+            }
+
+        }
+    }
+}
