013283e9dcb0c8dba1f5d8bd45203338feaef60e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:54:28 +0200

    Add files via upload

---
+++ 03.Arrays/More-Exercise/3.RoundingNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();// turn the console numbers to array
+
+            int[] roundedNums = new int[nums.Length];//int collection take nums and store them to array,define array
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);// round away from zero style.
+                Console.WriteLine($"{Convert.ToDecimal(nums[i])} => {Convert.ToDecimal(roundedNums[i])}");//Convert.ToDecimal i minava v judge
+                
+               // Console.WriteLine($"{nums[i]} => {Math.Round(nums[i], MidpointRounding.AwayFromZero)}");
+            }
+
+
+        }
+    }
+}
