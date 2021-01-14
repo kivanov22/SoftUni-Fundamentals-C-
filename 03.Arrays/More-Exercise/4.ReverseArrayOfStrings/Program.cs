ea593d8b73034f4eeef9a4b24fc31696d52a31ea
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:55:11 +0200

    Add files via upload

---
+++ 03.Arrays/More-Exercise/4.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(' ',Console.ReadLine()
+                .Split().Reverse()));
+
+            //v konzola za izpisvane slaga string join za da dobavi space, sled tova prochita ot konzolata
+            //sled tova gi razdelq i gi obrushta s reverse.
+        }
+    }
+}
