4b19d2db7d58ac376d1e9f7636c4f8a8e89b1c5f
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:52:40 +0200

    Add files via upload

---
+++ 03.Arrays/More-Exercise/2.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2.PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];//array for storing numbers
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+
+                numbers[i] = number;
+
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)//cikul za izpisvane na chislata v obraten red
+            {
+                Console.Write(numbers[i]+ " ");
+            }
+        }
+    }
+}
