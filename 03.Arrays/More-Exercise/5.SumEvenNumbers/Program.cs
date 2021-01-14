03c513b3f45a911ab6a587a9e97f6779acba6d63
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:56:21 +0200

    Add files via upload

---
+++ 03.Arrays/More-Exercise/5.SumEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an array to store the numbers
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;//prazna promenliva da suhranqva sumata
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];//promenliva skladirashta chislata
+
+                if (currentNumber%2==0)//proverka dali e chetno
+                {
+                    sum += currentNumber;//dobavqme chetnoto v sumata 
+                }
+
+            }
+            Console.WriteLine(sum);//printirame sumata na chetnite
+        }
+    }
+}
