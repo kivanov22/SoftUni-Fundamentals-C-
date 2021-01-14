1c9c77a6e27f0dfe188aaa7f3fd045fb9e897f5a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:46:42 +0200

    Add files via upload

---
+++ 03.Arrays/Lab/3.ZigZagPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.ZigZagPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];//empty in arrays
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+               int[] currentLine= Console.ReadLine().Split().Select(int.Parse).ToArray();// we get from console numbers from string to int
+
+                if (i % 2 ==0)//check if even or odd
+                {
+                    arr1[i] = currentLine[0];//take first element in arr1
+                    arr2[i] = currentLine[1];//take second element in arr2
+                }
+                else//swap the elements in the array we get from console
+                {
+                    arr1[i] = currentLine[1];//take the second element in arr1
+                    arr2[i] = currentLine[0];//take the first element in arr2
+                }
+
+            }
+            Console.WriteLine(string.Join(' ', arr1));//print arrays with space
+            Console.WriteLine(string.Join(' ', arr2));
+        }
+    }
+}
