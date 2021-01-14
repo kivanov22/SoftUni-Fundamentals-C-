2f72122670236b34ca006f682e08fcb050dc0f7a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-14 18:45:19 +0200

    Add files via upload

---
+++ 03.Arrays/Lab/1.TrainPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.TrainPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[n];//empty array
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());//directly giving the numbers in the empty array
+                 sum = wagons.Sum();//summing the numbers in the arrray
+            }
+            Console.WriteLine(String.Join(' ',wagons));//print the array with space
+            Console.WriteLine(sum);
+        }
+    }
+}
