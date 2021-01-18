3f7f333cf426994a564f89fe7665f80da892d45e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-18 20:33:37 +0200

    Add files via upload

---
+++ 03.Arrays/Exercise/1.Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[n];//array taking n from console
+            int sum = 0;
+
+            for (int i = 0; i < wagons.Length; i++)//array length 
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                sum += wagons[i];
+            }
+            Console.WriteLine(string.Join(" " ,wagons));
+            Console.WriteLine(sum);
+            //Console.WriteLine(wagons.Sum()); Linq method
+        }
+    }
+}
