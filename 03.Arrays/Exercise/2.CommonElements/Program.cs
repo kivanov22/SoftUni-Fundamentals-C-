98bab17cfe4b32742810d8dc959f8001d4f521b9
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-18 20:34:33 +0200

    Add files via upload

---
+++ 03.Arrays/Exercise/2.CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split().ToArray();//get two string arrays
+            string[] arr2 = Console.ReadLine().Split().ToArray();
+
+            string word = "";//storage for the word 
+
+            foreach (var item in arr2)// go through second array
+            {
+                if (arr1.Contains(item))//check first array for word
+                {
+                    word += " " + item;//get the word
+                  //  Console.Write($"{item}");
+                }
+            }
+            Console.Write($"{word.Trim()}");//print word without white-space
+        }
+    }
+}
