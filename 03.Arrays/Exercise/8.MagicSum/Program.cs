351a36bdab575af89a6d722c04b1ec3cbaf838a2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-22 14:36:40 +0200

    Add files via upload

---
+++ 03.Arrays/Exercise/8.MagicSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <arr.Length; i++)
+            {
+                int currentNumber = arr[i];// taking the current number from array
+
+                for (int j = i+1; j < arr.Length; j++)// for taking the next element
+                {
+                    int secondNumber = arr[j];//taking the second number from array
+
+                    if (currentNumber + secondNumber==number)//check if the sum of both numbers gives the number 
+                    {
+                        Console.WriteLine($"{currentNumber} {secondNumber}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
