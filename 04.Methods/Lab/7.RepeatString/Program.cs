b7894fbfd13ded754a30057b97ca2e7cac18d11c
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:21:57 +0200

    Add files via upload

---
+++ 04.Methods/Lab/7.RepeatString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _7.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            string result = RepeatString(word, count);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString (string word,int count)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(word);
+            }
+            return result.ToString();
+        }
+    }
+}
