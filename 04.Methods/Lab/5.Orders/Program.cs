d7995e98bbaa8bfc40156c8a6edadac7299713ea
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:20:50 +0200

    Add files via upload

---
+++ 04.Methods/Lab/5.Orders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _5.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            double amount = double.Parse(Console.ReadLine());
+            PrintProducts(product, amount);
+
+        }
+
+
+        static void PrintProducts(string product,double amount)
+        {
+            double price = 0;
+            double sum = 0;
+
+            switch (product)
+            {
+                case "water":
+                    price = 1.00;
+                    break;
+                case "coffee":
+                    price = 1.50;
+                    break;
+                case "coke":
+                    price = 1.40;
+                    break;
+                case "snacks":
+                    price = 2.00;
+                    break;
+                
+            }
+            sum = price*amount;
+            Console.WriteLine($"{sum:f2}");
+            
+        }
+    }
+}
