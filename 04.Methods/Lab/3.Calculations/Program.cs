1e6be1ea95fa77151658362b143de82c4ca11e9d
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:19:39 +0200

    Add files via upload

---
+++ 04.Methods/Lab/3.Calculations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _3.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            //add, multiply, subtract, divide
+
+            switch (action)
+            {
+                case "add":
+                    Add(number1, number2);
+                    break;
+                case "subtract":
+                    Subtract(number1, number2);
+                    break;
+                case "multiply":
+                    Multiply(number1, number2);
+                    break;
+                case "divide":
+                    Divide(number1, number2);
+                    break;
+                
+            }
+        }
+
+        // 4 methods for the 4 actions 
+        static void Add(int number1 , int number2)
+        {
+            Console.WriteLine(number1+number2);
+        }
+
+        static void Subtract(int number1 , int number2)
+        {
+            Console.WriteLine(number1-number2);
+        }
+        static void Multiply(int number1, int number2)
+        {
+            Console.WriteLine(number1*number2);
+        }
+        static void Divide (int number1 , int number2)
+        {
+            Console.WriteLine(number1/number2);
+        }
+    }
+}
