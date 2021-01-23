4bfbffb4c3df6da8234f130b4ae32eab01d9f3d8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:21:24 +0200

    Add files via upload

---
+++ 04.Methods/Lab/6.CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(width, height);// we created a variable for storing the method, so we can access the new value
+            Console.WriteLine(area);
+        }
+
+
+        static double GetRectangleArea(double width , double height)//double instead of void 
+        {
+            return width * height;//return so we can pass this to the first method
+        }
+    }
+}
