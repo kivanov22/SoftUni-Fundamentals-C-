0aa67633475266964d1d5955c9216bf1aa14df38
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:20:14 +0200

    Add files via upload

---
+++ 04.Methods/Lab/4.PrintingTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _4.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTriangle(n);// we call the method for the triangle
+
+
+        }
+
+        static void PrintLine(int start, int end)//method for printing numbers from start to end
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void PrintTriangle(int n)//method for Triangle
+        {
+            for (int line = 1; line <=n; line++)
+            {
+                PrintLine(1, line);
+              
+            }
+
+
+
+            for (int line= n - 1; line >= 1; line--)
+            {
+                PrintLine(1, line);
+                
+            }
+        }
+    }
+}
