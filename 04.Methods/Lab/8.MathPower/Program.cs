cac5d525f90b02ef031ba93a4622cf9c9179574a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-23 18:28:04 +0200

    Add files via upload

---
+++ 04.Methods/Lab/8.MathPower/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+
+        static double RaiseToPower(double number,double power)
+        {
+            double result = 0d;
+
+           result= Math.Pow(number, power);
+
+
+            return result;
+        }
+    }
+}
