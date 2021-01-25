19f5741c4401007d0c5e3658e6c65bbfa0078cf1
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-25 17:22:52 +0200

    Add files via upload

---
+++ 04.Methods/Lab/11.MathOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+
+           double total= Calculation(a, operation, b);
+            Console.WriteLine($"{total}");
+            
+
+        }
+
+        private static double Calculation(int a, string operation, int b)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+               }
+
+
+
+            return result;
+        }
+    }
+}
