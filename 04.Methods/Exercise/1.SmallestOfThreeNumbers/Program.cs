01aab23be2d3cfe090a9af373ff2bcd6071f27e1
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-25 17:24:06 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/1.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+            SmallestNumber(num1, num2, num3);
+
+        }
+
+        static void SmallestNumber(double num1 , double num2, double num3)
+        {
+            if (num1<num2&&num1<num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2<num1 && num2<num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3<num1 && num3<num2)
+            {
+                Console.WriteLine(num3);
+            }
+        }
+
+    }
+}
