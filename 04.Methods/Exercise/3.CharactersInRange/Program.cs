8910cfe00e88e17550c7515283829fcd17020ad9
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-25 17:25:21 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/3.CharactersInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            PrintBetween(firstChar, secondChar);
+        }
+
+        private static void PrintBetween(char firstChar, char secondChar)
+        {
+            if (firstChar>secondChar)
+            {
+                char first = firstChar;//take first char 
+                firstChar = secondChar;//swap char a,b = b,a
+                secondChar = first;//we save ourselfs second loop
+            }
+
+            for (int i = firstChar+1; i < secondChar; i++)// loop for goign through the chars betweeen first and second
+            {
+                Console.Write((char)i+ " ");// cast the index to char
+            }
+        }
+    }
+}
