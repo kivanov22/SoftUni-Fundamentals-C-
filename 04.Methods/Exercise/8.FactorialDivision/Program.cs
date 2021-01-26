fa7bcbf89671a653826d2e54e14c2ddffd8d3bb8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-26 22:11:36 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/8.FactorialDivision/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _8.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            double firstFactorial = GetFactorial(num1);
+            double secondFactorial = GetFactorial(num2);
+            double result = firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static double GetFactorial(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
+//double result = 1;
+//while (number != 1)
+//{
+//    result = result * number;
+//    number = number - 1;
+//}
+////return result;
