0668efc266dfb244b24b96eb7d4280a8941a0f3d
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-26 15:08:10 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/5.AddAndSubstract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5.AddAndSubstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int sum = PrintSum(first, second, third);
+            Console.WriteLine(sum);
+        }
+
+        static int PrintSum(int first,int second,int third)//summing numbers and returning method for Substract 
+        {
+            int sumBothNumbers = first+second;
+            return Substract(sumBothNumbers, third);
+        }
+
+        private static int Substract(int sumBothNumbers, int third)//Substract method giving the value to PrintSum
+        {
+            return sumBothNumbers - third;
+        }
+    }
+}
