9df4c1f8189384507904b038817f223ec7590880
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-26 22:12:20 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/9.PalindromeIntegers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace _9.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            IsitPalindrome(input);
+
+
+        }
+
+        private static void IsitPalindrome(string input)
+        {
+            string reversed = "";//saves the reversed string for example 123 to 321
+
+            while (input!="END")
+            {
+                input = Console.ReadLine();
+
+                if (input=="END")
+                {
+                    break;
+                }
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+                if (reversed!=input)
+                {
+                    Console.WriteLine("false");
+                }
+                else if (reversed==input)
+                {
+                    Console.WriteLine("true");
+                }
+                reversed = "";
+            }
+        }
+    }
+}
