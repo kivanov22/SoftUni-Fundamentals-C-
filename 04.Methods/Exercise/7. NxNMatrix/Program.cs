7aec677f0b7bd61d67e012954209632633fa7de0
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2021-01-26 22:10:55 +0200

    Add files via upload

---
+++ 04.Methods/Exercise/7. NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7._NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);// we dont need anything to return only print from console
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)//two loops for numbers
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n +" ");//print n with space
+                }
+                Console.WriteLine();//extra line
+            }
+        }
+    }
+}
