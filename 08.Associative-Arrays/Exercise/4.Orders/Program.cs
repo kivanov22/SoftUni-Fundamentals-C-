68f61b800b68617a6c2ff855040a40309067208d
Kristian Ivanov <krisko512@gmail.com>
2021-02-04 10:06:40 +0200

    Associative Arrays Exercise

---
+++ 08.Associative-Arrays/Exercise/4.Orders/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+
+            string input = Console.ReadLine();
+
+            while (input!="buy")
+            {
+                string[] currentProduct = input.Split();
+                string productName = currentProduct[0];
+                double productPrice =double.Parse(currentProduct[1]);
+                int productQuantity = int.Parse(currentProduct[2]);
+
+                if (!products.ContainsKey(productName))
+                {
+                    products.Add(productName, new List<double> { productPrice, productQuantity });
+                }
+                else
+                {
+                    products[productName][0] = productPrice;//index 0 is productPrice/index 1 is ProductQuantity
+                    products[productName][1] += productQuantity;//we set the index because we can't take double to list
+                    
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in products)
+            {
+                double total = item.Value[0] * item.Value[1];//find total from the new list by indexes
+
+                Console.WriteLine($"{item.Key} -> {total:f2}");
+            }
+        }
+    }
+}
---
+++ 08.Associative-Arrays/Exercise/9.ForceBook/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9.ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> forceRegister = new Dictionary<string,string>();
+
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Lumpawaroo")
+            {
+                if (commands.Contains("|"))
+                {
+                    string[] cmdArgs = commands.Split(" | ");
+                    string side = cmdArgs[0];
+                    string name = cmdArgs[1];
+
+                    if (!forceRegister.ContainsKey(name))
+                    {
+                        forceRegister.Add(name,side);//switched the values side,name
+                    }
+                    //if (!forceRegister[side].Contains(name))
+                    //{
+                    //    forceRegister[side].Add(name);
+                    //}
+                    //forceRegister[side].Add(name);
+                }
+                else if (commands.Contains("->"))
+                {
+                    string[] cmdArgs = commands.Split(" -> ");
+                    string name = cmdArgs[0];
+                    string side = cmdArgs[1];
+
+                    if (forceRegister.ContainsKey(name))
+                    {
+                       
+                        forceRegister[name] = side;
+                    }
+                    else
+                    {
+                        forceRegister.Add(name, side);
+                       
+                    }
+                    Console.WriteLine($"{name} joins the {side} side!");
+
+                }
+
+
+                commands = Console.ReadLine();
+            }
+
+            foreach (var users in forceRegister
+               .GroupBy(x => x.Value)
+               .OrderByDescending(x => x.Count())
+               .ThenBy(c => c.Key))
+            {
+                Console.WriteLine($"Side: {users.Key}, Members: {users.Count()}");
+
+                foreach (var elm in users.OrderBy(t => t.Key))
+                {
+                    Console.WriteLine($"! {elm.Key}");
+                }
+            }
+
+        }
+    }
+}
