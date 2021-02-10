0ae949586244bce6729a0bdccbe2fad8ccc238cf
Kristian Ivanov <krisko512@gmail.com>
2021-02-10 15:24:04 +0200

    Final Exam

---
+++ 11.Final-Exam/Final-Exam/checkPractical/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace checkPractical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
