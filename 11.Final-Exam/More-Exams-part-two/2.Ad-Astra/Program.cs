bf52b609951b3344de20b990cc3411dc0bcec311
Kristian Ivanov <kriskopk@abv.bg>
2021-02-17 18:58:59 +0200

    Ad Astra

---
+++ 11.Final-Exam/More-Exams-part-two/2.Ad-Astra/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2.Ad_Astra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[#\|])(?<name>[A-Za-z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})*\1(?<calories>\d{1,5})\1
+            //string pattern = @"([#\|])(?<food>[A-Z][a-z\s]+)(?<date>\d{2}\d{2}\d{2})(?<caloeries>[0-9]+)\1";
+            string newPattern = @"([#\|])(?<food>[A-Za-z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})*\1(?<calories>\d{1,5})\1";
+                                                                             
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, newPattern);
+
+            int totalCalories = 0;
+            int dailyCalories = 2000;
+            int days = 0;
+
+            foreach (Match item in matches)
+            {
+                // string device = match.Groups[1].Value;
+               
+                int calories = int.Parse(item.Groups["calories"].Value);
+
+                totalCalories += calories;
+                days = totalCalories / dailyCalories;
+                    
+            }
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match match in matches)
+            {
+                string food = match.Groups["food"].Value;
+                string date = match.Groups["date"].Value;
+                int calories = int.Parse(match.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {food}, Best before: {date}, Nutrition: {calories}");
+            }
+        }
+    }
+}
